a304b35188e7a4681ce8e132d634a7dbfb6da684
sora33 <shuuuya0616@gmail.com>
2023-12-28 17:34:25 +0000

    アバター画像を編集するAPIを実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[index show create update] do
         resource :skills, only: %i[show update]
       end
+      resource :avatars, only: [:update]
     end
   end
 end
