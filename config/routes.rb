31b3674d5453fee3de454a967cb3ea8409b21abc
sora33 <shuuuya0616@gmail.com>
2024-01-01 09:22:55 +0000

    特定ユーザーの投稿一覧のapi routeを実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       resources :users, only: %i[index show create update destroy] do
         resource :skills, only: %i[show update]
+        resources :posts, only: %i[index], controller: 'users/posts'
       end
       resource :avatars, only: [:update]
       resources :posts, only: %i[index create destroy]
