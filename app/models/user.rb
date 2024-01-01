0519eadb3ce5b161ad4b9aba2f55442a683ec9cc
sora33 <shuuuya0616@gmail.com>
2024-01-01 09:22:32 +0000

    ユーザーモデルにバリデーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   has_many :skills, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_one_attached :avatar
+
+  validates :provider, :provider_id, :name, presence: true
 end
